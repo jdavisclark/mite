20b2bc0163dd63fa5354e2ef57396da3662e8cbf
Davis Clark <jdc0589@gmail.com>
2014-03-09 17:50:47 -0500

    in progress submodule work...

--- lib/data/providers/mysql/queries/createMigrationsTable.sql
+++ lib/data/providers/mysql/queries/createMigrationsTable.sql
@@ -1,7 +1,7 @@
 CREATE  TABLE `_migration` (
 	`key` VARCHAR(255) NOT NULL COMMENT 'Migration Key.  Matches the filename of the migrations' ,
 	`hash` CHAR(40) NOT NULL COMMENT 'A hash value of tworkhe contents of the migration.' ,
-	`submodule` varchar(255) COMMENT 'name of the submodule the migration originated from. null for root',
-	PRIMARY KEY (`key`) ,
-	UNIQUE INDEX `key_UNIQUE` (`key` ASC)
+	`submodule` varchar(255) DEFAULT '.' COMMENT 'name of the submodule the migration originated from. null for root',
+	PRIMARY KEY (`key`, `submodule`),
+	UNIQUE INDEX `key_UNIQUE` (`key` ASC, `submodule` ASC)
 );
